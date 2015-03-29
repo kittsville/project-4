cfdf7e45653334f56fbf0aae20bc473f585992df
Kit <kittsville@googlemail.com>
2015-03-29 16:38:29 +0100

    Added order controller
    Created template order controller with order page and order creation functions

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,2 +1,11 @@
 class OrderController < ApplicationController
+  # Displays order page with restaurants/items to choose from
+  def orderPage
+    
+  end
+  
+  # Creates an order from given chosen items
+  def createOrder
+  
+  end
 end
