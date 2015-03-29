2c1c2ecba69983a7de1e8e4b3fead306bb6cfeef
Kit <kittsville@googlemail.com>
2015-03-29 18:49:43 +0100

    Fixed items columns
    Removed duplicate column cost in favour of price
    Changed price type from int to float, because money

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150329155605) do
+ActiveRecord::Schema.define(version: 20150329174519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,8 +34,7 @@ ActiveRecord::Schema.define(version: 20150329155605) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
-    t.integer  "price"
-    t.integer  "cost"
+    t.float    "price"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.integer  "restaurant_id"
