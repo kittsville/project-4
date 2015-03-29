364cf3163c20883f842608aa14176eb7a233160b
Kit <kittsville@users.noreply.github.com>
2015-03-30 00:30:56 +0100

    Fixed branch merge
    Please remove the >>HEAD before committing next time :(

--- config/routes.rb
+++ config/routes.rb
@@ -1,66 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  resources :items
-
-  resources :users
-  
-  get '/driver_login' => 'driver#login'
-  get '/' => 'driver#home'
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-=======
   # Only allows AJAX requests for a specific route
   class OnlyAjaxRequests
     def matches?(request)
@@ -72,5 +10,6 @@ Rails.application.routes.draw do
   
   match '/order/submit' => 'order#new_order', :constraints => OnlyAjaxRequests.new, via: :post, defaults: {format: 'json'}
   match '/order/get_details' => 'order#get_details', :constraints => OnlyAjaxRequests.new, via: :post, defaults: {format: 'json'}
->>>>>>> 3135889dc33f605798f26cd5eb9c1ff1c232ad99
+  
+  get '/driver_login' => 'driver#login'
 end
